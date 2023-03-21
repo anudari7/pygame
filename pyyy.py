2b92d4fa40812a97bfbc670267dd84511b43d149
Anuka <anukaganbold44@gmail.com>
2023-03-21 14:43:38 +0100

    haha

--- pyyy.py
+++ pyyy.py
@@ -3,12 +3,14 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((1100, 600))
+pygame.display.set_caption("Jumper")
 
-pygame.display.set_caption("byt namn")
+clock = pygame.time.Clock()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     pygame.display.update()
-
+    clock.tick(60)
