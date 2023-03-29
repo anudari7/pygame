8fef03a39ad396d425de5eace62efc8424a62fbf
Anuka <anukaganbold44@gmail.com>
2023-03-29 08:53:22 +0200

    haha

--- pyyy.py
+++ pyyy.py
@@ -7,10 +7,15 @@ pygame.display.set_caption("Jumper")
 
 clock = pygame.time.Clock()
 
+test_surface = pygame.image.load("graphics/Sky.png")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface, (200, 100))
+    
     pygame.display.update()
     clock.tick(60)
